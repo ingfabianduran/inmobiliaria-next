2202312dbc60e638b681703a1e887f488653592b
Fabian Duran <ingfabianavellaneda@gmail.com>
2022-02-03 12:39:48 -0500

    Commit temporal

--- pages/arrendadores/index.js
+++ pages/arrendadores/index.js
@@ -1,7 +1,7 @@
 import { PagePersona } from '../../components/RolPersona/PagePersona';
 import { getPersonas } from '../../api/serverSide';
 
-export default function Home({ dataTable }) {
+export default function Home({ tipoPersona, dataTable }) {
   return (
     <>
       <PagePersona>
@@ -14,6 +14,7 @@ export async function getServerSideProps(context) {
   getPersonas(context);
   return {
     props: {
+      tipoPersona: '',
       dataTable: []
     }
   }
